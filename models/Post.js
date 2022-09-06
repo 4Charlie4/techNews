9ab52c13cbca5aabd33e6bf9c2b896a4c2f43c4c
Charles Duran <62945929+4Charlie4@users.noreply.github.com>
2022-09-06 11:01:59 -0400

    update

--- models/Post.js
+++ models/Post.js
@@ -2,7 +2,7 @@ const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
 
 class Post extends Model {
-  //method that updates votes 
+  //method that updates votes
   static upvote(body, models) {
     return models.Vote.create({
       user_id: body.user_id,
